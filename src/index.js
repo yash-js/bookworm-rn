1aead6603601850f75ee0b0b2082f362e48a8698
Yash <yashpurani8@gmail.com>
2025-05-02 23:46:59 +0530

    limit added

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
