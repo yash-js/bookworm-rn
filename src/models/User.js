6f6cf52eceadb479ba9cb76ece2c6fc9e7af5c4d
Yash <yashpurani8@gmail.com>
2025-05-02 17:15:13 +0530

    fix

--- src/models/User.js
+++ src/models/User.js
@@ -5,17 +5,17 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      require: true,
+      required: true,
       unique: true,
     },
     email: {
       type: String,
-      require: true,
+      required: true,
       unique: true,
     },
     password: {
       type: String,
-      require: true,
+      required: true,
       minlength: 6,
     },
     profileImage: {
